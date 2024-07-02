af6924f134a24516ad50bef1be46301f0803061b
AlLNaumov <allnaumob@inno.tech>
2024-07-02 12:18:04 +0300

    Hello World+1

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,7 +4,8 @@ public class MainApplication {
      public static void main (String[] args) {
          System.out.println("Hello World");
      System.out.println(11000000111);
+         System.out.println("Hello World");
          System.out.println(111111111);
-         System.out.println(12121);
+         System.out.println("Hello World" );
      }
 }
