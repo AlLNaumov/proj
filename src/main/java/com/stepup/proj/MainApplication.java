f95a155161fbc7feb542773fd51206ae1fa16c97
AlLNaumov <allnaumob@inno.tech>
2024-07-02 11:22:49 +0300

    Что-то сделал

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -5,6 +5,7 @@ public class MainApplication {
          System.out.println("Hello World");
      System.out.println(11111);
          System.out.println(11111);
-         System.out.println(11111);
+         System.out.println(1122221);
+         System.out.println(110001);
      }
 }
