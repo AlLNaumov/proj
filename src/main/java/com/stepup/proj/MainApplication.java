5cf2b7ebf36db799877bdf2b7d10a5d05f7c8402
AlLNaumov <allnaumob@inno.tech>
2024-07-02 11:27:27 +0300

    Что-то сделал другой

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,8 +3,8 @@ package com.stepup.proj;
 public class MainApplication {
      public static void main (String[] args) {
          System.out.println("Hello World");
-     System.out.println(11111);
-         System.out.println(11111);
-         System.out.println(11111);
+     System.out.println(11000000111);
+         System.out.println(111111111);
+         System.out.println(12121);
      }
 }
